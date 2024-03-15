d1802fb959f20ad30fc286fe40cc3ee6eb984866
Artur Durasiewicz <artur@durasiewi.cz>
2024-03-15 14:11:35 +0100

    Working simple spending summary by categories.

--- src/category_rules.rs
+++ src/category_rules.rs
@@ -1,12 +1,22 @@
 use std::fs::File;
-use std::io::Read;
 use std::path::PathBuf;
-use serde::{Deserialize, Serialize};
+
+use regex::Regex;
+use serde::Deserialize;
+
+fn default_regex() -> Regex {
+    Regex::new("").unwrap()
+}
 
 #[derive(Deserialize)]
 pub struct MatchRule {
     pub transaction_type: String,
+    #[serde(skip, default = "default_regex")]
+    pub transaction_type_compiled: Regex,
+
     pub transaction_description: String,
+    #[serde(skip, default = "default_regex")]
+    pub transaction_description_compiled: Regex,
 }
 
 #[derive(Deserialize)]
@@ -21,8 +31,39 @@ pub struct CategoryRules {
     pub ignore: Vec<CategoryRule>,
 }
 
+fn compile_rule_match(mut rule_match: MatchRule) -> MatchRule {
+    if !rule_match.transaction_type.is_empty() {
+        rule_match.transaction_type_compiled = Regex::new(&rule_match.transaction_type).unwrap();
+    }
+
+    if !rule_match.transaction_description.is_empty() {
+        rule_match.transaction_description_compiled = Regex::new(&rule_match.transaction_description).unwrap();
+    }
+
+    return rule_match;
+}
+
 pub fn read_rules(file_path: &PathBuf) -> CategoryRules {
-    let mut file = File::open(file_path).unwrap();
-    return serde_json::from_reader(&file).unwrap();
+    let file = File::open(file_path).unwrap();
+    let content: CategoryRules = serde_json::from_reader(&file).unwrap();
+
+    let result = CategoryRules
+    {
+        rules: content.rules.into_iter()
+            .map(|mut x| {
+                x.match_rules = x.match_rules.into_iter().map(|item| compile_rule_match(item)).collect();
+                return x;
+            })
+            .collect(),
+
+        ignore: content.ignore.into_iter()
+            .map(|mut x| {
+                x.match_rules = x.match_rules.into_iter().map(|item| compile_rule_match(item)).collect();
+                return x;
+            })
+            .collect(),
+    };
+
+    return result;
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 use clap::Parser;
 use crate::account_history::read_history;
@@ -16,15 +17,30 @@ struct Args {
     category_rules: Option<PathBuf>,
 }
 
+
+
 fn main() {
     let args = Args::parse();
 
     let category_rules = args.category_rules.unwrap_or_else(|| PathBuf::from("rules.json"));
     let rules = read_rules(&category_rules);
     let history = read_history(&args.account_history);
+    let mut categories_summary: HashMap<String, f64> = HashMap::new();
+
     for item in history.operations.operations.iter() {
-        println!("{}", item.description);
+        for rule in rules.rules.iter() {
+            if rule.match_rules.iter().any(|q| q.transaction_description_compiled.is_match(&item.description)) {
+                let key = String::from(rule.category_name.as_str());
+                categories_summary.entry(key.clone()).or_insert(0.0);
+                categories_summary.entry(key).and_modify(|e| *e += item.amount);
+            }
+        }
     }
 
-    println!("Hello, world!");
+    println!("Account: {}", history.search.account);
+    println!("Since: {} To: {} \n", history.search.date.since, history.search.date.to);
+
+    for entry in categories_summary.iter() {
+        println!("{}: {}", entry.0, format!("{:.2}", entry.1.abs()));
+    }
 }
