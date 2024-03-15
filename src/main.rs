8cc61ce745b504dd767c6d5d5fe60d779f8ad454
Artur Durasiewicz <artur@durasiewi.cz>
2024-03-15 10:36:00 +0100

    Rules structures and reading them from json file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::Read;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MatchRule {
+    transaction_type: String,
+    transaction_description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CategoryRule {
+    category_name: String,
+    match_rules: Vec<MatchRule>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Rules {
+    rules: Vec<CategoryRule>,
+    ignore: Vec<CategoryRule>,
+}
+
 fn main() {
+    let mut file = File::open("rules.json").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let rules : Rules = serde_json::from_str(&contents).unwrap();
+
     println!("Hello, world!");
 }
